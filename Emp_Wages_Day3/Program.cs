537d1edf75a88eed13226917d9bfa60d44154b4c
Pavan699 <pavannakate03@gmail.com>
2021-09-03 02:29:02 -0700

    Calculate Month Wage

--- Emp_Wages_Day3/Program.cs
+++ Emp_Wages_Day3/Program.cs
@@ -9,7 +9,7 @@ namespace Emp_Wages_Day3
             Console.WriteLine("Welcome to Employee Wages using Switch-Case Day-3");
             Random emp_random = new Random();
             int empcheck = emp_random.Next(0, 3);
-            int oneDay_Sal = 0, full_Time = 8, perHour_Pay = 20, part_Time = 4;
+            int oneDay_Sal = 0, full_Time = 8, perHour_Pay = 20, part_Time = 4, total_Sal = 0, days = 20;
             switch (empcheck)
             {
                 case 1:
@@ -26,6 +26,8 @@ namespace Emp_Wages_Day3
                     Console.WriteLine(" Wrong Input :)");
                     break;
             }
+            total_Sal = oneDay_Sal * days;
+            Console.WriteLine("Total Work-Days Salary is : {0}", total_Sal);
         }
     }
 }
