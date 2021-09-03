4d10c427df06f4fa1bb4d8413f3f619bd234c1a5
Pavan699 <pavannakate03@gmail.com>
2021-09-03 01:57:30 -0700

    Present OR Absent_UC1

--- Emp_Wages_Day3/Program.cs
+++ Emp_Wages_Day3/Program.cs
@@ -7,6 +7,16 @@ namespace Emp_Wages_Day3
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wages using Switch-Case Day-3");
+            Random emp_random = new Random();
+            int empcheck = emp_random.Next(0, 2);
+            if (empcheck == 1)
+            {
+                Console.WriteLine("Employee is Present ");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent ");
+            }
         }
     }
 }
