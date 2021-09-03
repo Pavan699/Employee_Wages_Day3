cb12c5ec3ea79e97ae7941accbf9cdc6b59d1a47
Pavan699 <pavannakate03@gmail.com>
2021-09-03 02:13:10 -0700

    Part-Time Wage UC-3

--- Emp_Wages_Day3/Program.cs
+++ Emp_Wages_Day3/Program.cs
@@ -9,7 +9,7 @@ namespace Emp_Wages_Day3
             Console.WriteLine("Welcome to Employee Wages using Switch-Case Day-3");
             Random emp_random = new Random();
             int empcheck = emp_random.Next(0, 2);
-            int oneDay_Sal = 0, full_Time = 8, perHour_Pay = 20 ;
+            int oneDay_Sal = 0, full_Time = 8, perHour_Pay = 20, part_Time = 4;
             if (empcheck == 1)
             {
                 Console.WriteLine("It's Work-Type is : Full-Time");
@@ -18,7 +18,9 @@ namespace Emp_Wages_Day3
             }
             else
             {
-                Console.WriteLine("Employee is Absent ");
+                Console.WriteLine("It's Work-Type is : Part-Time");
+                oneDay_Sal = part_Time * perHour_Pay;
+                Console.WriteLine("One Day Salary is : {0}", oneDay_Sal);
             }
         }
     }
