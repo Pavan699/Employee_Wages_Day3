c831e4d6e5b06517bf3965b974c5b868fde29f42
Pavan699 <pavannakate03@gmail.com>
2021-09-03 01:49:10 -0700

    Emp_Wages Title

---
+++ Emp_Wages_Day3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Emp_Wages_Day3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wages using Switch-Case Day-3");
+        }
+    }
+}
