5d148bf1c965ec4e6f0a5cf3d666a4c73c0a194e
Pavan699 <pavannakate03@gmail.com>
2021-09-03 02:20:26 -0700

    Adding UC4-Switch-case Logic

--- Emp_Wages_Day3/Program.cs
+++ Emp_Wages_Day3/Program.cs
@@ -8,19 +8,23 @@ namespace Emp_Wages_Day3
         {
             Console.WriteLine("Welcome to Employee Wages using Switch-Case Day-3");
             Random emp_random = new Random();
-            int empcheck = emp_random.Next(0, 2);
+            int empcheck = emp_random.Next(0, 3);
             int oneDay_Sal = 0, full_Time = 8, perHour_Pay = 20, part_Time = 4;
-            if (empcheck == 1)
+            switch (empcheck)
             {
-                Console.WriteLine("It's Work-Type is : Full-Time");
-                oneDay_Sal = full_Time * perHour_Pay;
-                Console.WriteLine("One Day Salary is : {0}", oneDay_Sal);
-            }
-            else
-            {
-                Console.WriteLine("It's Work-Type is : Part-Time");
-                oneDay_Sal = part_Time * perHour_Pay;
-                Console.WriteLine("One Day Salary is : {0}", oneDay_Sal);
+                case 1:
+                    Console.WriteLine("It's Work-Type is : Full-Time");
+                    oneDay_Sal = full_Time * perHour_Pay;
+                    Console.WriteLine("One Day Salary is : {0}", oneDay_Sal);
+                    break;
+                case 2:
+                    Console.WriteLine("It's Work-Type is : Part-Time");
+                    oneDay_Sal = part_Time * perHour_Pay;
+                    Console.WriteLine("One Day Salary is : {0}", oneDay_Sal);
+                    break;
+                default:
+                    Console.WriteLine(" Wrong Input :)");
+                    break;
             }
         }
     }
